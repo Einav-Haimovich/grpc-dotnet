fb20071d509d9bdd388ae0e3d08d7f2b2f298848
Einav Haimovich <einav@haimol.net>
2026-03-07 23:08:03 +0200

    My First GRPC Server

---
+++ gRPC-In-Dotnet/gRPC-In-Dotnet/Services/FirstService.cs
@@ -0,0 +1,71 @@
+﻿using Basics;
+using Grpc.Core;
+
+namespace gRPC_In_Dotnet.Services
+{
+    public class FirstService : FirstServiceDefinition.FirstServiceDefinitionBase
+    {
+        private readonly ILogger<FirstService> _logger;
+
+        public FirstService(ILogger<FirstService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<Response> Unary(Request request, ServerCallContext context)
+        {
+            _logger.LogInformation($"Received unary request: {request.Content}");
+
+            var response = new Response
+            {
+                Message = $"Hello {request.Content} from server"
+            };
+
+            return Task.FromResult(response);
+        }
+
+        public override async Task<Response> ClientStreaming(IAsyncStreamReader<Request> requestStream, ServerCallContext context)
+        {
+            var response = new Response
+            {
+                Message = "Server got client stream"
+            };
+
+            while (await requestStream.MoveNext())
+            {
+                var request = requestStream.Current;
+                _logger.LogInformation($"Received message: {request.Content}");
+                response.Message += $"\nReceived message: {request.Content}";
+            }
+
+            return response;
+        }
+
+        public override async Task ServerStreaming(Request request, IServerStreamWriter<Response> responseStream, ServerCallContext context)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var response = new Response
+                {
+                    Message = $"Hello {request.Content} from server stream {i + 1}"
+                };
+                await responseStream.WriteAsync(response);
+            }
+        }
+
+        public override async Task BidirectionalStreaming(IAsyncStreamReader<Request> requestStream, IServerStreamWriter<Response> responseStream, ServerCallContext context)
+        {
+            var response = new Response
+            {
+                Message = "Server got bidirectional stream"
+            };
+            while (await requestStream.MoveNext())
+            {
+                var request = requestStream.Current;
+                _logger.LogInformation($"Received message: {request.Content}");
+                response.Message += $"\nReceived message: {request.Content}";
+                await responseStream.WriteAsync(response);
+            }
+        }
+    }
+}
